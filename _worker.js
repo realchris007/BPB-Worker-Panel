5366cb598d0b51d0edf426d34da58d6f5ccb9b21
realchris007 <46042722+realchris007@users.noreply.github.com>
2024-08-02 12:21:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '25645a7d-0d4f-4f16-8bda-7c0ba4336d9c';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
